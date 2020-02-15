9ae0700b8fbc5e0458f73f4245a673f2a2451cb5
yuhanyin <blossomyyh@163.com>
2020-02-15 08:19:45 -0800

    ejs

--- app.js
+++ app.js
@@ -3,11 +3,11 @@ var app = express();
 var request = require("request")
 var mongoose = require("mongoose");
 var bodyParser = require("body-parser");
+const ejs = require("ejs");
 app.set("view engine","ejs");
 
 
 app.use(bodyParser.urlencoded({extended:true}));
-// mongoose.connect("mongodb://localhost/jourme");
 mongoose.connect('mongodb://localhost/jourme', { useNewUrlParser: true , useUnifiedTopology: true});
 
 //connect to mongodb
@@ -16,9 +16,18 @@ mongoose.connect('mongodb://localhost/jourme', { useNewUrlParser: true , useUnif
 //   res.render("welcome");
 // });
 
+let today = new Date();
+let todoList = ["Sleep","Eat","?"];
+
 app.get("/",function(req,res){
-  res.render("welcome");
+  res.render("welcome",  {
+    day: today,
+    todoList: todoList
+  } );
 });
 app.listen(3000,function(){
   console.log('Server has started!');
 });
+
+
+
