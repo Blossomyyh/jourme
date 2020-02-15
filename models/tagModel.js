2636f69a98e15216786cfe8324349e0adaf48984
VivianVKJ <1067937107@qq.com>
2020-02-15 06:58:33 -0800

    model

---
+++ models/tagModel.js
@@ -0,0 +1,12 @@
+import mongoose from 'mongoose';
+const Schema = mongoose.Schema;
+
+export const UserTagSchema = new Schema({
+  userTagName: String,
+  description: String,
+});
+
+export const InputTagSchema = new Schema({
+  userTagName: String,
+  description: String,
+});
