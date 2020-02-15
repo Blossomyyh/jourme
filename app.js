e7909d80ec06e0558a07e0ed12aa7b939f6a7a8e
yuhanyin <blossomyyh@163.com>
2020-02-15 10:45:45 -0800

    start

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 var express = require("express");
 var app = express();
+const path = require('path');
+const routes = require('./routes');
+
 var request = require("request")
 var mongoose = require("mongoose");
 var bodyParser = require("body-parser");
 const ejs = require("ejs");
-app.set("view engine","ejs");
 
 
 app.use(bodyParser.urlencoded({extended:true}));
@@ -16,15 +18,17 @@ mongoose.connect('mongodb://localhost/jourme', { useNewUrlParser: true , useUnif
 //   res.render("welcome");
 // });
 
-let today = new Date();
-let todoList = ["Sleep","Eat","?"];
 
-app.get("/",function(req,res){
-  res.render("welcome",  {
-    day: today,
-    todoList: todoList
-  } );
-});
+
+// Set the default views directory to html folder
+app.set('views', path.join(__dirname, 'views'));
+// Set the folder for css & java scriptss
+app.use(express.static(path.join(__dirname,'css')));
+app.use(express.static(path.join(__dirname, 'node_modules')));
+
+app.set("view engine","ejs");
+
+app.use('/', routes);
 app.listen(3000,function(){
   console.log('Server has started!');
 });
---
+++ routes/index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const router = express.Router();
+let today = new Date();
+let todoList = ["Sleep","Eat","?"];
+
+router.get("/",function(req,res){
+  res.render("welcome",  {
+    day: today,
+    todoList: todoList
+  } );
+});
+
+// router.get('/', (req, res) => {
+//   console.log('Request for home recieved');
+//   res.render('welcome');
+// });
+
+router.get('/person', (req, res) => {
+  console.log('Request for about page recieved');
+  res.render('person',  {
+    day: today,
+    todoList: todoList
+  } );
+});
+
+router.get('/start', (req, res) => {
+    console.log('Request for about page recieved');
+    res.render('start',  {
+      day: today,
+      todoList: todoList
+    } );
+  });
+
+
+module.exports = router;
